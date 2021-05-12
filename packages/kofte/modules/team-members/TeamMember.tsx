4ff7d5b15c6dd052658456d422d69bb9d41ff8f1
kamal <kbomoi@yahoo.com>
2021-05-13 00:33:16 +0300

    owner icon

--- packages/kofte/modules/team-members/TeamMember.tsx
+++ packages/kofte/modules/team-members/TeamMember.tsx
@@ -2,15 +2,21 @@ import React from "react";
 import { ITeamMember } from "@app/water";
 import { ListItem, ListIcon } from "@chakra-ui/react";
 import { AiOutlineUser } from "react-icons/ai";
+import { FaCrown } from "react-icons/fa";
 
 interface Props {
   member: ITeamMember;
+  teamOwnerId: string;
 }
 
-export const TeamMember: React.FC<Props> = ({ member }) => {
+export const TeamMember: React.FC<Props> = ({ member, teamOwnerId }) => {
   return (
-    <ListItem>
-      <ListIcon as={AiOutlineUser} color="blue.300" />
+    <ListItem sx={{ display: "flex", alignItems: "center" }}>
+      <ListIcon
+        as={teamOwnerId === member.user.id ? FaCrown : AiOutlineUser}
+        color="blue.300"
+        sx={{ display: "flex", alignItems: "center" }}
+      />
       {member.user.email}
     </ListItem>
   );
